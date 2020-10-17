94f6cde123d34948a7075ac98f5cca9ddfa21da7
Jonathan Palacio <jpalacio0612@gmail.com>
2020-10-16 19:12:50 -0500

    Connection with mongoAtla succesful

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const db = require('./src/db');
 
 const port = process.env.PORT || 8000;
 
 const app = express();
 
+db();
 app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
---
+++ src/db.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const { url } = require('./key');
+
+function db() {
+  console.log(url);
+  const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+
+  mongoose.connect(url, options);
+
+  const { connection } = mongoose;
+
+  connection.once('open', () =>
+    console.log('Connection established sucessful'),
+  );
+  connection.on('error', (error) => console.log('Something went wrong'));
+
+  return connection;
+}
+
+module.exports = db;
